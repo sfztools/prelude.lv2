176abd3f6ccabe10699275b8c7b9fd6c3b4ca40c
Paul Fd <paul@ferrand.cc>
2021-02-27 11:05:19 +0100

    Added a license file

--- prelude.c
+++ prelude.c
@@ -12,7 +12,7 @@
   Copyright 2011 Gabriel M. Beddingfield <gabriel@teuton.org>
   Copyright 2011 James Morris <jwm.art.net@gmail.com>
 
-  Permission to use, copy, modify, and/or distribute this software for any
+  Permission to use, copy, modify, and distribute this software for any
   purpose with or without fee is hereby granted, provided that the above
   copyright notice and this permission notice appear in all copies.
 
